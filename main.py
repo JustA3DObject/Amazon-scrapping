b142d696839297ba314a6c5411f9af55cbbc1e68
Aayush <just.another.3d.object@gmail.com>
2022-07-20 18:00:30 +0530

    Created list of urls

--- main.py
+++ main.py
@@ -29,5 +29,11 @@ def make_country_list():
 
 make_asin_list()
 make_country_list()
-print(asin_list)
-print(country_list)
+
+url_list = []
+i = 0
+for asin, country in (zip(asin_list, country_list)):
+    url_list.insert(i, f"https://www.amazon.{country}/dp/{asin}")
+    i = i+1
+
+print(url_list)
