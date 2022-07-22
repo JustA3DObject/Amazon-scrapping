6d3bba80f331d5b558efc1e41684d4db95fff95a
Aayush <just.another.3d.object@gmail.com>
2022-07-22 17:53:29 +0530

    Refactored code

--- main.py
+++ main.py
@@ -45,18 +45,25 @@ def make_url_list():
 make_url_list()
 # print(url_list)
 
-j = 100
-while(j < 200):
+header = ["Product", "Price", "Details", "Image URL"]
+with open("AmazonScraperData.csv", 'w', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
 
-    try:
-        URL = url_list[j]
-        j = j+1
-        # headers from https://httpbin.org/get
+j = 0
+while(j < 10):
 
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
-                   "X-Amzn-Trace-Id": "Root=1-62d8036d-2b173c1f2e4e7a416cc9e554", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                   "Accept-Encoding": "gzip, deflate, br", }
-        page = requests.get(URL, headers=headers)
+    URL = url_list[j]
+    j = j+1
+    # headers from https://httpbin.org/get
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
+               "X-Amzn-Trace-Id": "Root=1-62d8036d-2b173c1f2e4e7a416cc9e554", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+               "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-GB", }
+
+    page = requests.get(URL, headers=headers)
+
+    if page.status_code == 200:
         soup1 = BeautifulSoup(page.content, "html.parser")
         soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
         title = soup2.find(id="productTitle").get_text()
@@ -64,25 +71,21 @@ while(j < 200):
         about = soup2.find(
             class_="a-unordered-list a-vertical a-spacing-mini").get_text()
         img_url = soup2.find(id="landingImage")
-
         title = title.strip()
         price = price.strip()
         about = about.strip()
         img_url = img_url['src'].strip()
 
-    except AttributeError:
-        print("404 ERROR!")
-        title = "404 ERROR!"
-
-        header = ["Product"]
-        data = [title]
+        data = [title, price, about, img_url]
         with open("AmazonScraperData.csv", 'a+', newline='', encoding='UTF8') as f:
             writer = csv.writer(f)
             writer.writerow(data)
 
     else:
-        header = ["Product", "Price", "Details", "Image URL"]
-        data = [title, price, about, img_url]
+        print("404 ERROR!")
+        title = "404 ERROR!"
+
+        data = [title]
         with open("AmazonScraperData.csv", 'a+', newline='', encoding='UTF8') as f:
             writer = csv.writer(f)
             writer.writerow(data)
